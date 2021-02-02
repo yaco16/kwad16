dc4dcd099b94e0300aa18d5b9d9262a1634cf81f
yaco16 <65079827+yaco16@users.noreply.github.com>
2021-02-02 14:49:14 +0100

    update nav add cross animation on burger

--- src/components/Nav/app.js
+++ src/components/Nav/app.js
@@ -1,15 +1,19 @@
 /* eslint-disable import/prefer-default-export */
 /* eslint-disable no-console */
 export const navSlide = () => {
-  console.log('là');
   const nav = document.querySelector('.nav__links');
   const navLinks = document.querySelectorAll('.nav__links li');
-  console.log('navLinks:', navLinks);
+  const burger = document.querySelector('.burger');
 
   nav.classList.toggle('nav-active');
 
   navLinks.forEach((link, index) => {
-    link.style.animation = `navLinkFade 0.5s ease forwards ${index / navLinks.length + 0.5}s`;
-    console.log(index / navLinks.length + 0.2);
+    if (link.style.animation) {
+      link.style.animation = '';
+    } else {
+      link.style.animation = `navLinkFade 0.5s ease forwards ${index / navLinks.length + 0.5}s`;
+    }
   });
+
+  burger.classList.toggle('toggle');
 };
