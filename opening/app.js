b693f5d2df3e60884521c555041682fdda4adf0e
yaco16 <65079827+yaco16@users.noreply.github.com>
2021-02-02 01:07:40 +0100

    add enter link on opening add animation add link

--- opening/app.js
+++ opening/app.js
@@ -6,3 +6,12 @@ const logo = document.querySelectorAll("svg path");
 for (let i = 0 ; i < logo.length ; i++) {
   console.log(`Letter ${i} is ${logo[i].getTotalLength()}`)
 }
+
+// window.onload = function() {
+//   setTimeout(showMessage, 3000)
+// };
+
+const showMessage = () => {
+  console.log('je suis dans showMessage');
+  document.getElementById('opening__enter').setAttribute('style', 'display: block');
+};
