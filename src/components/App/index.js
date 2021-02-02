77b586a6db44c4d7fc7a3e370e98e6c0e2652d37
yaco16 <65079827+yaco16@users.noreply.github.com>
2021-02-02 01:55:18 +0100

    add Error404 component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
 import Opening from 'src/components/Opening';
+import Error404 from 'src/components/Error404';
 
 const App = () => (
   <div className="app">
-    <Opening />
+    <Switch>
+      <Route exact path="/">
+        <Opening />
+      </Route>
+      <Route>
+        <Error404 />
+      </Route>
+    </Switch>
   </div>
 );
 
---
+++ src/components/Error404/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './style.scss';
+
+const Error404 = () => (
+  <main>
+    <div className="fourzerofour">
+      <div className="fourzerofour__blank__div" />
+      <h1 className="fourzerofour__number"><span>4</span>0<span>4</span></h1>
+      <p className="fourzerofour__text">Not Found</p>
+      <div className="fourzerofour__blank__div" />
+    </div>
+  </main>
+);
+
+export default Error404;
