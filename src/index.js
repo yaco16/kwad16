6ac6a15f8e4028278b1c7cf2fb6abb7f9571a5e0
yaco16 <65079827+yaco16@users.noreply.github.com>
2021-02-02 01:38:41 +0100

    add BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import App from 'src/components/App';
 
-const rootReactElement = <App />;
+const rootReactElement = (
+  <BrowserRouter>
+    <App />;
+  </BrowserRouter>
+);
+
 const target = document.getElementById('root');
 render(rootReactElement, target);
