27621f11a9c1f2b4a8e5708bb8539362407d6fec
yaco16 <65079827+yaco16@users.noreply.github.com>
2021-02-12 00:42:10 +0100

    update Cards add images

--- public/data/projects.js
+++ public/data/projects.js
@@ -1,17 +1,24 @@
 const projects = [
   {
     id: 1,
-    name: 'Project 1',
-    title: 'Title 1',
-    description: 'Description 1',
+    image: 'pimz',
+    title: 'Pim\'Z',
+    description: 'This is the one-month end-of-O\'Clock formation project I worked on with three more developers',
     url: 'url 1',
-    technos: ['REact', 'Redux', 'Node', 'Express', 'Postregsql', 'AWS'],
+    technos: ['React', 'Redux', 'Node', 'Express', 'Postregsql', 'AWS'],
   },
   {
     id: 2,
-    name: 'Project 2',
-    title: 'Title 2',
-    description: 'Description 2',
+    image: 'kwad16',
+    title: 'kwad16.fr',
+    description: 'This is the website you`re currently browsing',
+    url: '#',
+  },
+  {
+    id: 3,
+    image: 'todo',
+    title: 'To-do list',
+    description: 'This is one example of the exercices I studied during the React specialization lessons',
     url: 'url 2',
   },
 ];
--- src/Pages/projects.js
+++ src/Pages/projects.js
@@ -5,6 +5,7 @@ import './style.scss';
 const Projects = () => (
   <main>
     <h1 className="page__title">Here is the list of projects I work on</h1>
+    <p>Hover the cards to learn more.</p>
     <Cards />
   </main>
 );
