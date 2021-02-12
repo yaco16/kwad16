9af584c7a991efa102183aaa8bc5ae999d547d0c
Yannik Coadou <kwad16@gmail.com>
2021-02-12 16:37:58 +0100

    update userController add login method check if email exists ok compare passwords with bcrypt ok

--- server_back/controllers/userController.js
+++ server_back/controllers/userController.js
@@ -4,21 +4,44 @@ const bcrypt = require('bcrypt');
 const db = require('../database');
 
 const userController = {
-
-  create: async (req, res) => {
+  createUser: async (req, res) => {
     try {
-      const emailSearched = await db.query('SELECT * FROM "user" WHERE email=$1;', [
-        req.body.email,
-      ]);
+      const emailSearched = await db.query(
+        'SELECT * FROM "user" WHERE email=$1;',
+        [req.body.email]
+      );
       console.log('emailSearched:', emailSearched.rows[0]);
       if (emailSearched.rows[0] !== undefined) {
         res.status(200).json('Mail already used');
       } else {
         const hashedPassword = await bcrypt.hash(req.body.password, 10);
-        await db.query('INSERT INTO "user" (username, email, password) VALUES ($1, $2, $3);', [
-          req.body.username, req.body.email, hashedPassword,
-        ]);
+        await db.query(
+          'INSERT INTO "user" (username, email, password) VALUES ($1, $2, $3);',
+          [req.body.username, req.body.email, hashedPassword],
+        );
         res.status(200).json('New user created');
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  },
+
+  login: async (req, res) => {
+    try {
+      const emailSearched = await db.query(
+        'SELECT * FROM "user" WHERE email=$1;',
+        [req.body.email],
+      );
+      if (emailSearched.rows[0] === undefined) {
+        res.status(200).json('Email unknown');
+        return;
+      }
+
+      const checkPassword = await bcrypt.compare(req.body.password, emailSearched.rows[0].password);
+      if (!checkPassword) {
+        res.status(200).json('Wrong password');
+      } else {
+        res.status(200).json(true);
       }
     } catch (error) {
       console.log(error);
--- server_back/router.js
+++ server_back/router.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = new express.Router();
 const userController = require('./controllers/userController');
 
-router.post('/signUp', userController.create);
+router.post('/signUp', userController.createUser);
+router.post('/login', userController.login);
 
 module.exports = router;
