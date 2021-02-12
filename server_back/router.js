8a01a026e1cf3e4a6c62f9076f8c393e3baec5ec
Yannik Coadou <kwad16@gmail.com>
2021-02-12 14:39:22 +0100

    update router add signup post route

--- server_back/router.js
+++ server_back/router.js
@@ -1,4 +1,7 @@
 const express = require('express');
 const router = new express.Router();
+const userController = require('./controllers/userController');
+
+router.post('/signUp', userController.create);
 
 module.exports = router;
