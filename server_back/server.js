0c5213ccfe22797ba727494aaed4a783081c52d5
Yannik Coadou <kwad16@gmail.com>
2021-02-12 14:40:17 +0100

    update server add bodyparser

--- server_back/server.js
+++ server_back/server.js
@@ -1,12 +1,24 @@
 /* eslint-disable no-console */
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 const router = require('./router');
 
 const app = express();
-const PORT = process.env.PORT || 3500;
+const PORT = process.env.PORT || 3500;
+
+app.use(
+  cors({
+    origin: '*',
+  }),
+);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.use(router);
+
 app.listen(PORT, () => {
   console.log(`Back server started on port ${PORT}`);
 });
