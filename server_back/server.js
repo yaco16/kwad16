6df7c0ffa56f3210b05129cd20b235426e82a5f5
Yannik Coadou <kwad16@gmail.com>
2021-02-12 12:06:44 +0100

    add server

---
+++ server_back/server.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-console */
+require('dotenv').config();
+const express = require('express');
+// const router = require('./app/router');
+const app = express();
+const PORT = process.env.PORT || 3500;
+
+// app.use(router);
+app.listen(PORT, () => {
+  console.log(`Back server started on port ${PORT}`);
+});
