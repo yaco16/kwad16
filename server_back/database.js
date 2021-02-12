f6b41ca9ce0a0ccf12d74b78540209f44311b934
Yannik Coadou <kwad16@gmail.com>
2021-02-12 13:11:48 +0100

    add database connexion

---
+++ server_back/database.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-console */
+const { Pool } = require('pg');
+require('dotenv').config();
+
+const client = new Pool({
+  host: process.env.PG_HOST,
+  port: process.env.PG_PORT,
+  database: process.env.PG_DATABASE,
+  user: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
+});
+
+(async () => {
+  await client.connect();
+})();
+
+// eslint-disable-next-line quotes
+client.query('SELECT NOW()', (err, res) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('Connected to database successfully');
+  }
+  client.end();
+});
+
+module.exports = client;
