b82bf5bd8a3de84fa2de004b09756851d6d12cd8
Yannik Coadou <kwad16@gmail.com>
2021-02-12 12:15:10 +0100

    add router

--- server_back/server.js
+++ server_back/server.js
@@ -1,11 +1,12 @@
 /* eslint-disable no-console */
 require('dotenv').config();
 const express = require('express');
-// const router = require('./app/router');
+const router = require('./router');
+
 const app = express();
 const PORT = process.env.PORT || 3500;
 
-// app.use(router);
+app.use(router);
 app.listen(PORT, () => {
   console.log(`Back server started on port ${PORT}`);
 });
