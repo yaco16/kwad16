4fa21558c6fb9211d3cc4022f475d2acf209ef04
Yannik Coadou <kwad16@gmail.com>
2021-02-12 16:20:44 +0100

    update userController add password crypting with Bcrypt

--- server_back/controllers/userController.js
+++ server_back/controllers/userController.js
@@ -6,7 +6,6 @@ const db = require('../database');
 const userController = {
 
   create: async (req, res) => {
-    console.log('req:', req.body.email);
     try {
       const emailSearched = await db.query('SELECT * FROM "user" WHERE email=$1;', [
         req.body.email,
@@ -15,12 +14,12 @@ const userController = {
       if (emailSearched.rows[0] !== undefined) {
         res.status(200).json('Mail already used');
       } else {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
         await db.query('INSERT INTO "user" (username, email, password) VALUES ($1, $2, $3);', [
-          req.body.username, req.body.email, req.body.password,
+          req.body.username, req.body.email, hashedPassword,
         ]);
         res.status(200).json('New user created');
       }
-      // const hashedPassword = bcrypt.hash(req.body.password, 10);
     } catch (error) {
       console.log(error);
     }
