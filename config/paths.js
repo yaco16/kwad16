1f1c0f2e7fcd054304dff7dfcf25ce5fd114918f
yaco16 <65079827+yaco16@users.noreply.github.com>
2021-02-12 03:02:39 +0100

    update Card change path of images

--- config/paths.js
+++ config/paths.js
@@ -2,7 +2,6 @@ const path = require('path');
 
 module.exports = {
   src: path.resolve(__dirname, '../src'), // source files
-  public: path.resolve(__dirname, '../public'), // source files
   assets: path.resolve(__dirname, '../src/assets'), // assets files
   build: path.resolve(__dirname, '../dist'), // production build files
   static: path.resolve(__dirname, '../public'), // static files to copy to build folder
--- src/components/Cards/card.js
+++ src/components/Cards/card.js
@@ -4,9 +4,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import './style.scss';
-import pimz from '../../../public/img/pimz.png';
-import kwad16 from '../../../public/img/kwad16.png';
-import todo from '../../../public/img/todo.png';
+import pimz from 'src/../public/img/pimz.png';
+import kwad16 from 'src/../public/img/kwad16.png';
+import todo from 'src/../public/img/todo.png';
 
 // import handleClic from './card_logic';
 
