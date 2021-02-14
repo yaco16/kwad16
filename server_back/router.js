8c01e8d5a565a1687c629f40e038f518542b3ee3
yaco16 <65079827+yaco16@users.noreply.github.com>
2021-02-14 14:57:56 +0100

    update router replace userController by mainController

--- server_back/router.js
+++ server_back/router.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = new express.Router();
 const userController = require('./controllers/userController');
+const mainController = require('./controllers/mainController');
 
 router.post('/signUp', userController.createUser);
 router.post('/login', userController.login);
-router.post('/submitMail', userController.submitMail);
+router.post('/submitMail', mainController.submitMail);
 
 module.exports = router;
